9416bac556bb61dcffb57b82faa5a4c3ade62405
Lang Sharpe <langer8191@gmail.com>
2011-12-10 15:47:13 +1000

    Update installer, just install any script in the config directory.

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/config"
-echo $CONFIG_DIR
 
-for file in ".vimrc" ".zshrc"
+for SRC_FILE in $( find $CONFIG_DIR -type f -depth 1 )
 do
-	ln -s $CONFIG_DIR/$file ~/
+  echo $SRC_FILE
+  DEST_FILE=~/$( basename $SRC_FILE)
+  if [ ! -e $DEST_FILE ]
+  then
+	  ln -s $SRC_FILE $DEST_FILE
+  fi
+  if [ ! -h $DEST_FILE ]
+  then
+    echo "Error: $DEST_FILE is not a symbolic link"
+  fi
 done

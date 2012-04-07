5859b66b10c706063bc3ac8e778ed6e390f7e09b
Lang Sharpe <langer8191@gmail.com>
2012-04-07 15:15:31 +1000

    Fix find call for different OS's.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/config"
 
 # Synlink in every file/directory in the config dir
-for SRC_FILE in $( find $CONFIG_DIR \( -type f -or -type d \) -and -depth 1 )
+for SRC_FILE in $( find $CONFIG_DIR -maxdepth 1 -and \( -type f -or -type d \) )
 do
   DEST_FILE=~/$( basename $SRC_FILE)
   # Careful not to overwrite anything that already exists

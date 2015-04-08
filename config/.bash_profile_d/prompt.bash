68421781683bf6f14ec81b13af49749547baca24
Lang Sharpe <langer8191@gmail.com>
2015-04-08 13:28:59 +1000

    Rename functions to match 'module name'

--- config/.bash_profile_d/prompt.bash
+++ config/.bash_profile_d/prompt.bash
@@ -1,12 +1,12 @@
-function parse_git_dirty {
+function prompt_git_dirty {
   [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]] && echo " ✗"
 }
 
-function parse_git_branch {
+function prompt_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"
 }
 
-PS1="${green}\w${bold_blue}(\$(parse_git_branch)\$(parse_git_dirty)${bold_blue})${reset_color} "
+PS1="${green}\w${bold_blue}(\$(prompt_git_branch)\$(prompt_git_dirty)${bold_blue})${reset_color} "
 
 # Set window title
 PS1="\[\033]0;\W\007\]$PS1"

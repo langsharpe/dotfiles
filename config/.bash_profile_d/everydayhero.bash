7683e03bab8dbf56d7fd04074300b55409ee23e9
Lang Sharpe <langer8191@gmail.com>
2016-02-02 08:44:21 +1000

    plain-utils aliases

--- config/.bash_profile_d/everydayhero.bash
+++ config/.bash_profile_d/everydayhero.bash
@@ -4,3 +4,7 @@ alias run_admin_au='be script/server -p 3002'
 alias run_admin_uk='be script/server -p 3102'
 alias run_heroix_au='be script/server -p 3001'
 alias run_heroix_uk='be script/server -p 3101'
+
+alias pussh='plain-utils ssh'
+alias pur='plain-utils run'
+alias pu='plain-utils'

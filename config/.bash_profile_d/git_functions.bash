f3a786f05a62d1cf3cd8132cd273a91820a6f7f4
Lang Sharpe <langer8191@gmail.com>
2016-05-14 13:23:46 +1000

    Add git_recent_branches

--- config/.bash_profile_d/git_functions.bash
+++ config/.bash_profile_d/git_functions.bash
@@ -1,3 +1,13 @@
 function git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"
 }
+
+function git_recent_branches {
+  git reflog |                                              # recent git activity
+    sed -n 's/^.*checkout: moving from \(.*\) to.*$/\1/p' | # branch switches
+    sed -n '/[0-91-f]\{40\}/! p' |                          # not detatched commits
+    grep -ve "^master$" |                                   # not master
+    uniq                                                    # not duplicated
+}
+
+alias gre=git_recent_branches

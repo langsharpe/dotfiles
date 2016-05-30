c4445d07b2f02782ed3d5ec18f8e15a64725f151
Lang Sharpe <langer8191@gmail.com>
2016-05-31 09:42:02 +1000

    git_recent_branches limit to 10 items

--- config/.bash_profile_d/git_functions.bash
+++ config/.bash_profile_d/git_functions.bash
@@ -7,7 +7,8 @@ function git_recent_branches {
     sed -n 's/^.*checkout: moving from \(.*\) to.*$/\1/p' | # branch switches
     sed -n '/[0-91-f]\{40\}/! p' |                          # not detatched commits
     grep -ve "^master$" |                                   # not master
-    uniq                                                    # not duplicated
+    uniq |                                                  # not duplicated
+    head --lines 10                                         # only 10
 }
 
 alias gre=git_recent_branches

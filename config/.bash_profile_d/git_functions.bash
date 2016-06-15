ccd0e15ae70d10b2485e55578e11d932814208a7
Lang Sharpe <langer8191@gmail.com>
2016-06-15 17:00:25 +1000

    gre: Cope with opic branches

--- config/.bash_profile_d/git_functions.bash
+++ config/.bash_profile_d/git_functions.bash
@@ -3,12 +3,12 @@ function git_branch {
 }
 
 function git_recent_branches {
-  git reflog |                                              # recent git activity
-    sed -n 's/^.*checkout: moving from \(.*\) to.*$/\1/p' | # branch switches
-    sed -n '/[0-91-f]\{40\}/! p' |                          # not detatched commits
-    grep -ve "^master$" |                                   # not master
-    uniq |                                                  # not duplicated
-    head --lines 10                                         # only 10
+  git reflog |                                               # recent git activity
+    sed -n 's/^.*checkout: moving from \(.*\) to .*$/\1/p' | # branch switches
+    sed -n '/[0-91-f]\{40\}/! p' |                           # not detatched commits
+    grep -ve "^master$" |                                    # not master
+    uniq |                                                   # not duplicated
+    head --lines 10                                          # only 10
 }
 
 alias gre=git_recent_branches

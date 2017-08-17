493019fb0f9bd1e98e39c7f6ad06a22a12343da4
Lang Sharpe <langer8191@gmail.com>
2017-08-17 11:31:12 +1000

    More EDH apps moved to convox

--- config/.bash_profile_d/everydayhero.bash
+++ config/.bash_profile_d/everydayhero.bash
@@ -5,9 +5,9 @@ alias pu='plain-utils'
 alias tc='tagcumber -f progress -f rerun --out tmp/cucumber.rerun'
 alias tcre='tagcumber -f progress @tmp/cucumber.rerun'
 
-alias zuul-prod="plain-utils run zuul-prod bin/rails c"
+alias zuul-prod="convox run --app=zuul --rack=production web bin/rails c"
 alias payments-prod="plain-utils run payments-prod bin/rails c"
 alias zuul-staging="convox run --app=zuul --rack=staging web bin/rails c"
-alias payments-staging="plain-utils run payments-staging bin/rails c"
+alias payments-staging="convox run --app=payments --rack=production web bin/rails c"
 
 export DISABLE_SPRING=true

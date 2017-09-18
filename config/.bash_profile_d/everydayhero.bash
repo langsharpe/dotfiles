61e6d0471975bfdded3874970c6ddecf32839829
Lang Sharpe <langer8191@gmail.com>
2017-09-18 11:12:59 +1000

    Use convox @ everydayhero

--- config/.bash_profile_d/everydayhero.bash
+++ config/.bash_profile_d/everydayhero.bash
@@ -6,8 +6,8 @@ alias tc='tagcumber -f progress -f rerun --out tmp/cucumber.rerun'
 alias tcre='tagcumber -f progress @tmp/cucumber.rerun'
 
 alias zuul-prod="convox run --app=zuul --rack=production web bin/rails c"
-alias payments-prod="plain-utils run payments-prod bin/rails c"
+alias payments-prod="convox run --app=payments --rack=production web bin/rails c"
 alias zuul-staging="convox run --app=zuul --rack=staging web bin/rails c"
-alias payments-staging="convox run --app=payments --rack=production web bin/rails c"
+alias payments-staging="convox run --app=payments --rack=staging web bin/rails c"
 
 export DISABLE_SPRING=true

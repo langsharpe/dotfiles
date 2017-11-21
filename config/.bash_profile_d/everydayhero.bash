68a32a78bdfd44fe9a713feccb2aebd42911ad2a
Lang Sharpe <langer8191@gmail.com>
2017-11-21 13:09:50 +1000

    Add more everydayhero aliases

--- config/.bash_profile_d/everydayhero.bash
+++ config/.bash_profile_d/everydayhero.bash
@@ -5,9 +5,12 @@ alias pu='plain-utils'
 alias tc='tagcumber -f progress -f rerun --out tmp/cucumber.rerun'
 alias tcre='tagcumber -f progress @tmp/cucumber.rerun'
 
-alias zuul-prod="plain-utils run zuul-prod bin/rails c"
+alias zuul-prod="convox run --app=zuul --rack=production web bin/rails c"
 alias payments-prod="convox run --app=payments --rack=production web bin/rails c"
-alias zuul-staging="plain-utils run zuul-staging bin/rails c"
+alias supporter-prod="convox run --app=supporter --rack=production serve bin/rails c"
+
+alias zuul-staging="convox run --app=payments --rack=staging web bin/rails c"
 alias payments-staging="convox run --app=payments --rack=staging web bin/rails c"
+alias supporter-staging="convox run --app=supporter --rack=staging serve bin/rails c"
 
 export DISABLE_SPRING=true
